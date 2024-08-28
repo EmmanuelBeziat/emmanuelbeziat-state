cb1d5b7f0f3da02aca546ead7ca9ec118e5b9629
Emmanuel Béziat <social@emmanuelbeziat.com>
2024-08-28 17:46:55 +0200

    refactor: :art: Better Log.js class structure

--- src/models/Log.js
+++ src/models/Log.js
@@ -9,51 +9,64 @@ class Log {
 	}
 
 	/**
-   * Retrieves all files from the folder, reads their content, and returns it.
-   * @returns {Promise<Array>} A promise that resolves with the content of all files.
+   * Retrieves all valid log folders and their content.
+   * @returns {Promise<Array>} A promise that resolves with the content of all log files.
    */
   async getAllLogs () {
     try {
-			const folders = await fs.readdir(path.resolve(this.path), 'utf8')
-			if (!folders.length) throw new Error('No folders in folder')
-
-			const validFolders = await Promise.all(folders.map(async folder => {
-				const stats = await fs.stat(path.resolve(this.path, folder))
-				return stats.isDirectory() ? folder : null
-			}))
-
-			const logs = await Promise.all(validFolders.filter(Boolean).map(async folder => {
-				const logFilePath = path.join(folder, 'output.log')
-				try {
-					const content = await this.getLogContent(logFilePath)
-					if (!content) return null
-					return { name: folder, state: this.state.machine.current, content }
-				}
-				// Skip if folder is empty
-				catch (error) {
-					return null
-				}
-			})).then(results => results.filter(Boolean))
-
+			const folders = await this.getValidFolders()
+			const logs = await this.getLogsFromFolders(folders)
 			return logs
 		}
 		catch (error) {
-			throw new Error(error.message || 'An error occurred while retrieving logs')
+			throw new Error(error.message || 'Unable to read logs folders')
 		}
   }
 
 	/**
-   * Reads the content of a file, parses it, and returns the parsed content.
-   * @param {string} fileName The name of the file to read.
-   * @returns {Promise<Object>} A promise that resolves with the parsed content of the file.
-   */
-	async getLogContent (fileName) {
-    try {
-			const file = await fs.readFile(path.resolve(this.path, fileName), 'utf8')
+	 * Retrieves valid folders from the log directory.
+	 * @returns {Promise<Array>} A promise that resolves with an array of valid folder names.
+	 */
+	async getValidFolders () {
+		const folders = await fs.readdir(path.resolve(this.path), 'utf-8')
+		if (!folders.length) throw new Error('No log folders found')
+
+		const validFolders = await Promise.all(folders.map(async folder => {
+			const stats = await fs.stat(path.resolve(this.path, folder))
+			return stats.isDirectory() ? folder : null
+		}))
+
+		return validFolders.filter(Boolean)
+	}
+
+	/**
+	 * Retrieves logs from specified folders
+	 * @param {Array} folders An array of folder names.
+	 * @returns {Promise<Array>} A romise that resovles with the logs from the folders.
+	 */
+	async getLogsFromFolders (folders) {
+		const logs = await Promise.all(folders.map(async folder =>{
+			const logFilePath = path.join(folder, 'output.log')
+			return this.getLogContent(logFilePath)
+				.then(content => content ? { name: folder, state: this.state.machine.current, content} : null)
+				.catch(() => null) // Skip if folder is empty
+		}))
+
+		return logs.filter(Boolean)
+	}
+
+	/**
+	 * Reads the content of a file and returns it.
+	 * @param {string} filename Name of the file to read.
+	 * @returns {Promise<String>} A promise that resolves with the content of the file.
+	 */
+	async getLogContent (filename) {
+		try {
+			const file = await fs.readFile(path.resolve(this.path, filename), 'utf-8')
 			return file
 		}
 		catch (error) {
-			throw new Error(error.message || 'An error occurred while reading the file')
+			throw new Error(error.message || 'An error occurend while reading the log file')
 		}
 	}
 
