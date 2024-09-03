a76e3f6875c8f729680699ec51d53de863acc746
Emmanuel Béziat <social@emmanuelbeziat.com>
2024-09-03 04:15:36 +0200

    refactor: :art: Cleaner code and html

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,17 +1,21 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', () => {
-	document.querySelectorAll('.log').forEach(log => {
-		const opener = log.querySelector('.log-details-opener')
-		const dialog = log.querySelector('.log-details-modal')
-		const closer = log.querySelector('.log-details-closer')
+const modalHandler = log => {
+	const opener = log.querySelector('.log-details-opener')
+	const dialog = log.querySelector('.log-details-modal')
+	const closer = log.querySelector('.log-details-closer')
 
-		opener.addEventListener('click', () => { dialog.showModal() })
-		closer.addEventListener('click', () => { dialog.close() })
-		dialog.addEventListener('click', event => {
-			if (event.target.nodeName === 'DIALOG') { dialog.close() }
-		})
+	opener.addEventListener('click', () => { dialog.showModal() })
+	closer.addEventListener('click', () => { dialog.close() })
+	dialog.addEventListener('click', event => {
+		if (event.target.nodeName === 'DIALOG') { dialog.close() }
 	})
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+	// Modales
+	document.querySelectorAll('.log').forEach(log => { modalHandler(log) })
+
+	// Code highligting
 	hljs.highlightAll()
 })
